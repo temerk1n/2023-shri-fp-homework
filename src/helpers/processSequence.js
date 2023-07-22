ec7b5e7adb05444a78c97810f85475ec7e3e463c
temerk1n <nikita.verigin23@gmail.com>
2023-07-22 10:13:02 +0300

    feat: add solution

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -12,40 +12,99 @@
  * – to [Int] – в какую систему счисления
  *
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
- * Ответ будет приходить в поле {result}
- */
- import Api from '../tools/api';
+ * Ответ будет приходить в поле {result}*/
+import Api from '../tools/api';
+import {
+    allPass,
+    gt,
+    __,
+    andThen,
+    toString,
+    pipe,
+    ifElse,
+    prop,
+    lt, otherwise, tap
+} from "ramda";
+import {isNumber, toNumber} from "lodash/lang";
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const wait = time => new Promise(resolve => {
+    setTimeout(resolve, time);
+})
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const getLength = str => str.length;
+const gt0 = gt(__, 0);
+const gt2 = gt(__, 2);
+const lt10 = lt(__, 10);
+const isValidLength = pipe(
+    getLength,
+    allPass([gt2, lt10])
+);
+const isValidNumber = pipe(toNumber, isNumber);
+const isPositive = pipe(toNumber, gt0);
+const isValidInput = allPass([
+    isPositive,
+    isValidNumber,
+    isValidLength,
+]);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const roundValue = pipe(toNumber, Math.round);
+const pow2 = value => value * value;
+const mathMod3 = value => value % 3;
+const getBinaryNumberLength = pipe(toString, getLength);
+const getApiResponse = prop('result');
+const getBinaryValueFromApi = value => api.get('https://api.tech/numbers/base', {from: 10, to: 2, number: value});
+const getAnimalById = id => api.get(`https://animals.tech/${id}`, {});
+const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
+    const log = tap(writeLog);
+    // const curriedLog = curry(log);
+    // const logStartValue = curriedLog('Start value: ');
+    // const logRoundedValue = curriedLog('Rounded value: ');
+    // const logBinaryValue = curriedLog('Binary value: ');
+    // const logValueLength = curriedLog('Value length: ');
+    // const logSquareNumber = curriedLog('Square value: ');
+    // const logMod3 = curriedLog('Remainder: ');
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+    const handleValidationError = () => handleError("ValidationError");
+    const handleNetworkError = () => handleError("NetworkError");
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+    const roundValueAndLog = pipe(roundValue, log);
+    const getBinaryNumberFromApiResponse = pipe(getApiResponse, log);
+    const getBinaryNumberLengthAndLog = pipe(getBinaryNumberLength, log);
+    const pow2AndLog = pipe(pow2, log);
+    const mod3AndLog = pipe(mathMod3, log);
+    const getAnimalByIdFromApiAndLogSuccessOrError = pipe(
+        getAnimalById,
+        andThen(pipe(getApiResponse, handleSuccess)),
+        otherwise(handleNetworkError)
+    )
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+    const handleSuccessfulResponse = pipe(
+        getBinaryNumberFromApiResponse,
+        getBinaryNumberLengthAndLog,
+        pow2AndLog,
+        mod3AndLog,
+        getAnimalByIdFromApiAndLogSuccessOrError
+    );
+
+    const handleValidInput = pipe(
+        roundValueAndLog,
+        getBinaryValueFromApi,
+        andThen(handleSuccessfulResponse),
+        otherwise(handleNetworkError)
+    )
+
+    const process = pipe(
+        log,
+        ifElse(
+            isValidInput,
+            handleValidInput,
+            handleValidationError
+        )
+    )
+
+    process(value);
+}
 
 export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -12,39 +12,93 @@
  *
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
+import {allPass, anyPass, compose, count, curry, equals, prop} from "ramda";
+import {COLORS, SHAPES} from '../constants'
+
+const isWhite = equals(COLORS.WHITE);
+const isRed = equals(COLORS.RED);
+const isGreen = equals(COLORS.GREEN);
+const isBlue = equals(COLORS.BLUE);
+const isOrange = equals(COLORS.ORANGE);
+const isNotWhite = color => color !== COLORS.WHITE;
+const shapesHasSameColor = (shape1, shape2) => equals(shape1, shape2);
+const triangleColorEqualsSquare = shapes => shapesHasSameColor(getTriangleColor(shapes), getSquareColor(shapes));
+
+const getColor = shape => prop(shape);
+const getSquareColor = getColor(SHAPES.SQUARE);
+const getStarColor = getColor(SHAPES.STAR);
+const getCircleColor = getColor(SHAPES.CIRCLE);
+const getTriangleColor = getColor(SHAPES.TRIANGLE);
+
+const greater1 = count => count > 1;
+const less2 = count => count <= 1
+const equals1 = count => count === 1;
+const equals2 = count => count === 2;
+const equals4 = count => count === 4;
+
+const starIsRed = compose(isRed, getStarColor);
+const squareIsGreen = compose(isGreen, getSquareColor);
+const circleIsWhite = compose(isWhite, getCircleColor);
+const triangleIsWhite = compose(isWhite, getTriangleColor);
+const circleIsBlue = compose(isBlue, getCircleColor);
+const squareIsOrange = compose(isOrange, getSquareColor);
+const triangleIsGreen = compose(isGreen, getTriangleColor);
+const triangleIsNotWhite = compose(isNotWhite, getTriangleColor);
+const shapeIsNotRedOrWhite = anyPass([isBlue, isOrange, isGreen]);
+
+const currentColors = shapes => Object.values(shapes);
+const countColor = curry((colorFunc, shapes) => count(colorFunc, currentColors(shapes)));
+const countGreen = countColor(isGreen)
+const countRed = countColor(isRed);
+const countBlue = countColor(isBlue);
+const countWhite = countColor(isWhite);
+const countOrange = countColor(isOrange);
+const twoGreenShapes = compose(equals2, countGreen);
+const oneRedShape = compose(equals1, countRed);
+const redCountEqualsBlue = (shapes) => equals(countRed(shapes), countBlue(shapes));
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
-
-    return star === 'red' && square === 'green';
-};
+export const validateFieldN1 = allPass([
+    starIsRed,
+    squareIsGreen,
+    circleIsWhite,
+    triangleIsWhite
+])
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = compose(greater1, countGreen);
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = redCountEqualsBlue;
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 =  allPass([
+    circleIsBlue,
+    starIsRed,
+    squareIsOrange
+]);
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = compose(less2, countWhite);
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = allPass([
+    twoGreenShapes,
+    triangleIsGreen,
+    oneRedShape
+]);
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = compose(equals4, countOrange);
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = compose(shapeIsNotRedOrWhite, getStarColor);
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = compose(equals4, countGreen);
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = allPass([
+    triangleColorEqualsSquare,
+    triangleIsNotWhite
+]);
